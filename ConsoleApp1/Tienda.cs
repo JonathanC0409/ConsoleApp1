af263645be3103ca701990058f38cdee9d08e73a
JonathanC0409 <soyeljoni123@gmail.com>
2025-07-01 12:40:13 -0500

    agregar funcionalidad de venta en tiendas

---
+++ ConsoleApp1/Tienda.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Tienda
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Producto> Productos { get; set; }
+        public Tienda(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            Productos = new List<Producto>();
+        }
+        public void AddProducto(Producto producto)
+        {
+            Productos.Add(producto);
+        }
+        public void RemoveProducto(Producto producto)
+        {
+            Productos.Remove(producto);
+        }
+        public void DisplayProductos()
+        {
+            Console.WriteLine($"Productos in {Name}:");
+            foreach (var producto in Productos)
+            {
+                producto.Display();
+            }
+        }
+        public decimal TotalSales()
+        {
+            return Productos.Sum(p => p.TotalPrice());
+        }
+        public override string ToString()
+        {
+            return $"{Name} (ID: {Id}) - Total Sales: {TotalSales():C}";
+        }
+        public static Tienda Create(int id, string name)
+        {
+            return new Tienda(id, name);
+        }
+    }
+}
