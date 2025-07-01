e3fc0d7a3648f76c27d4ddc03c2a37f68d999c05
JonathanC0409 <soyeljoni123@gmail.com>
2025-07-01 12:34:30 -0500

    primeros ajustes de la aplicacion quenervios

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("This is a simple console application.");
+            Console.WriteLine("It demonstrates the use of C# 12 features.");
+            Console.WriteLine("Make sure to run it in a .NET 8 environment.");
         }
     }
 }
